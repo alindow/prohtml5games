0723b9c0eea1244af804e340bb9e9ef89c194fb4
Arne Lindow <info@arne-lindow.de>
2018-03-25 14:50:52 +0200

    Step 3: Level Select Screen

--- js/game.js
+++ js/game.js
@@ -8,9 +8,46 @@ var game = {
     game.canvas = $('#gamecanvas')[0];
     game.context = game.canvas.getContext('2d');
   },
+  showLevelScreen:function(){
+    $('.gamelayer').hide();
+    $('#levelselectscreen').show('slow');
+  },
 }
+
+var levels = {
+    // Level data
+    data: [{   // First level
+        foreground: "desert-foreground",
+        background: "clouds-background",
+        entities: []
+    }, {   // Second level
+        foreground: "desert-foreground",
+        background: "clouds-background",
+        entities: []
+    }],
+
+    // Initialize level selection screen
+    init:function(){
+      var html = "";
+      for (var i = 0; i < levels.data.length; i++) {
+        var level = levels.data[i];
+        html += '<input type = "button" value = "' + (i + 1) + '">';
+      };
+      $('#levelselectscreen').html(html);
+      // Set the button click event handlers to load level
+      $('#levelselectscreen input').click(function(){
+        levels.load(this.value-1);
+        $('#levelselectscreen').hide();
+      });
+    },
+    // Load all data and images for a specific level
+    load: function(number) {
+    }
+};
+
 
 // Intialize game once page has fully loaded
 window.addEventListener("load", function() {
     game.init();
+    levels.init();
 });
