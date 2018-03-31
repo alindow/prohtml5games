a34c4108c5538e4e2c150461b458b54af350586d
Arne Lindow <info@arne-lindow.de>
2018-03-31 07:54:55 +0200

    first empty world

--- box2d-demo.js
+++ box2d-demo.js
@@ -10,7 +10,11 @@ var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 var b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
 
 var world;
+var scale = 30;
 
 function init() {
-
+  // Set up the Box2d world that will do most of the physics calculation
+  var gravity = new b2Vec2(0,9.8); //declare gravity as 9.8 m/s^2 downward
+  var allowSleep = true; //Allow objects that are at rest to fall asleep and be excluded from calculations
+  world = new b2World(gravity,allowSleep);
 }
