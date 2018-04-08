2896c64826c0b4b3324f31df0e3049a3072c22d4
Arne Lindow <info@arne-lindow.de>
2018-04-08 18:43:23 +0200

    create method added to entity

--- js/game.js
+++ js/game.js
@@ -132,8 +132,55 @@ var entities = {
       restitution:0.4,
     }
   },
-  // take the entity, create a Box2D body, and add it to the world
-  create:function(entity){
+  // Take the entity, create a Box2D body, and add it to the world
+  create: function(entity) {
+      var definition = entities.definitions[entity.name];
+
+      if (!definition) {
+          console.log("Undefined entity name", entity.name);
+
+          return;
+      }
+
+      switch (entity.type) {
+          case "block": // simple rectangles
+              entity.health = definition.fullHealth;
+              entity.fullHealth = definition.fullHealth;
+              entity.shape = "rectangle";
+              entity.sprite = loader.loadImage("images/entities/" + entity.name + ".png");
+
+              entity.breakSound = game.breakSound[entity.name];
+
+              box2d.createRectangle(entity, definition);
+              break;
+          case "ground": // simple rectangles
+                  // No need for health. These are indestructible
+              entity.shape = "rectangle";
+                  // No need for sprites. These won't be drawn at all
+              box2d.createRectangle(entity, definition);
+              break;
+          case "hero":    // simple circles
+          case "villain": // can be circles or rectangles
+              entity.health = definition.fullHealth;
+              entity.fullHealth = definition.fullHealth;
+              entity.sprite = loader.loadImage("images/entities/" + entity.name + ".png");
+              entity.shape = definition.shape;
+
+              entity.bounceSound = game.bounceSound;
+
+              if (definition.shape === "circle") {
+                  entity.radius = definition.radius;
+                  box2d.createCircle(entity, definition);
+              } else if (definition.shape === "rectangle") {
+                  entity.width = definition.width;
+                  entity.height = definition.height;
+                  box2d.createRectangle(entity, definition);
+              }
+              break;
+          default:
+              console.log("Undefined entity type", entity.type);
+              break;
+      }
   },
   // take the entity, its position, and its angle and draw it on the game canvas
   draw:function(entity,position,angle){
